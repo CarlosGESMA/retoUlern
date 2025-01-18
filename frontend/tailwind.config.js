d49e415bff45d8b1f2fba1a1efb0aeacb7e78c53
CarlosSM <csanmartina3@gmail.com>
2025-01-18 17:50:18 -0300

    creación de front

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        customRed: '#e52026',
+        primaryRed: '#E42127',
+        secondaryRed: '#EF5350',
+        primaryGray: '#757575',
+        secondaryGray: '#3A505E',
+        bgRed: '#E42127',
+        bgInpuGray: '#ECEFF1',
+        bgGray: '#CFD8DC',
+        bgSelectedScroll: '#FFB1B1',
+        bgScroll: '#FFD9D9',
+    },
+    },
+  },
+  plugins: [],
+};
